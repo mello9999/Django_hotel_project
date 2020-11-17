70f2fd6d9e534ef53fd317decd26df9bdc4c75bd
bank1333 <natchapol.pat@gmail.com>
2020-11-17 13:06:34 +0700

    add database setting

--- project/settings.py
+++ project/settings.py
@@ -99,11 +99,14 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lab5',
+        'USER': 'postgres',
+        'PASSWORD': '*******',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
