739214f9e4999914a33f022275ca20bb99fd6727
Natchariya Wongamnuayporn <natchariya_kao@hotmail.com>
2020-12-14 09:46:57 +0700

    kaoaddinvoice_receipt_form

--- hotel_room.sql
+++ hotel_room.sql
@@ -1,7 +1,11 @@
 CREATE VIEW hotel_room AS
 	SELECT r.type as "room category" ,
-	   r."price" as "fare" ,
+	   r."price" as "room price" ,
+	   r."exprice" as "extra price" ,
 	   r."available" as "availability"
 FROM roomtype r ;
+
+DROP view hotel_room;
+
 
 SELECT * FROM hotel_room;
--- project/settings.py
+++ project/settings.py
@@ -106,15 +106,15 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+        'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hotel_management',
-        'USER': 'postgres',
-        'PASSWORD': '123456789',     
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': 'hotel',
+        'USER': 'natchapolpatamawisut@dd-dinner-dog',
+        'PASSWORD': 'Bank_1333',     
+        'HOST': 'dd-dinner-dog.postgres.database.azure.com', 
+        'PORT': '5432',
+        }
     }
-}
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
